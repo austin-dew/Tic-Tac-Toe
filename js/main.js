19003293290395669fe89510af76e982f543e2db
Austin <scentedpine0@gmail.com>
2022-07-05 18:08:04 -0600

    Completed checking for row win conditions

--- js/main.js
+++ js/main.js
@@ -2,8 +2,41 @@ let xOrOCounter = 0;
 let tiles = document.querySelectorAll('.tile');
 tiles.forEach((tile) => {
     tile.addEventListener('click', () => {
-        tile.children[0].innerText = xOrOCounter % 2 === 0 ? "X" : "O";
-        xOrOCounter++;
+        if(tile.children[0].innerText === '-') {
+            tile.children[0].innerText = xOrOCounter % 2 === 0 ? "X" : "O";
+            xOrOCounter++;
+        }
+        checkForWin(tiles)
     })
 })
 
+function checkForWin(tiles) {
+    //create 2d array 
+    let tiles2D = convertTilesTo2DArray(tiles);
+    //console.log(tiles2D);
+
+    //checks for rows
+    for(let i = 0; i < 3; i++) {
+        if(tiles2D[i].every(tile => tile === tiles2D[i][0] && tile !== '-')) {
+            console.log(`${tiles2D[i][0]}'s win!`)
+            return;
+        }
+    }
+}
+
+function convertTilesTo2DArray(tiles) {
+    let board = Array.from(tiles).map(tile => tile.innerText);
+    let tiles2D = [];
+    let inc = 0;
+
+    for(let i = 0; i < 3; i++) {
+        let row = [];
+        for(let j = inc; j < inc + 3; j++) {
+            row.push(board[j]);
+        }
+        tiles2D.push(row);
+        inc += 3;
+    }
+    
+    return tiles2D;
+}
