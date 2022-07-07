10584e770a7a5737655180f06bbe7ce058ff69d4
Austin <scentedpine0@gmail.com>
2022-07-06 18:16:30 -0600

    finished game, added win checking and reset button

--- js/main.js
+++ js/main.js
@@ -1,27 +1,65 @@
+document.querySelector('#resetBtn').addEventListener('click', resetBoard);
+
 let xOrOCounter = 0;
 let tiles = document.querySelectorAll('.tile');
+let boardState = document.querySelector('#boardState');
+let gameOver = false;
 tiles.forEach((tile) => {
     tile.addEventListener('click', () => {
-        if(tile.children[0].innerText === '-') {
+        if(tile.children[0].innerText === '-' && !gameOver) {
             tile.children[0].innerText = xOrOCounter % 2 === 0 ? "X" : "O";
+            boardState.innerText = `${xOrOCounter % 2 !== 0 ? "X" : "O"}'s turn`;
             xOrOCounter++;
         }
-        checkForWin(tiles)
+
+        gameOver = checkForWin(tiles)
     })
 })
 
 function checkForWin(tiles) {
     //create 2d array 
     let tiles2D = convertTilesTo2DArray(tiles);
-    //console.log(tiles2D);
+    let tiles2DCols = getColumnsFrom2DArray(tiles2D);
+    let isWin = true;
 
-    //checks for rows
+    // checks each element in the array inside the 2D array to see if they are same value
+    const winCheck = (arr, i) => { 
+        return arr[i].every(tile => tile === arr[i][0] && tile !== '-') 
+    } 
+
+    // checks columns and rows for wins
     for(let i = 0; i < 3; i++) {
-        if(tiles2D[i].every(tile => tile === tiles2D[i][0] && tile !== '-')) {
-            console.log(`${tiles2D[i][0]}'s win!`)
-            return;
+        if(winCheck(tiles2D, i)) { // checks rows
+            boardState.innerText = `${tiles2D[i][0]}'s win!`;
+            return isWin;
+        }
+        else if(winCheck(tiles2DCols, i)){ // checks columns
+            boardState.innerText = `${tiles2D[i][0]}'s win!`;
+            return isWin;
         }
     }
+
+    // checks to see if diagonal win is possible
+    if(tiles2D[1][1] !== '-') { 
+        if(tiles2D[0][0] === tiles2D[1][1] && tiles2D[1][1] === tiles2D[2][2]) { // checks tl mm br
+            boardState.innerText = `${tiles2D[0][0]}'s win!`;
+            return isWin;
+        }
+        else if(tiles2D[0][2] === tiles2D[1][1] && tiles2D[1][1] === tiles2D[2][0]) { // checks tr mm bl
+            boardState.innerText = `${tiles2D[0][2]}'s win!`;
+            return isWin;
+        }
+    }
+
+    return !isWin;
+}
+
+function getColumnsFrom2DArray(tiles) {
+    const getCol = i => { 
+        return tiles.map(row => { return row[i] })
+    };
+
+    return [getCol(0),getCol(1),getCol(2)]
 }
 
 function convertTilesTo2DArray(tiles) {
@@ -39,4 +77,13 @@ function convertTilesTo2DArray(tiles) {
     }
     
     return tiles2D;
+}
+
+function resetBoard() {
+    boardState.innerText = 'X\'s turn';
+    gameOver = false;
+    xOrOCounter = 0;
+    tiles.forEach(tile => {
+        tile.innerHTML = '<span>-</span>';
+    });
 }
